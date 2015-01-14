d4b9f21825e08bce7a7b125de1191cf9bdf644c1
jcbraun@uchicago.edu <jcbraun@uchicago.edu>
2015-01-14 01:21:42 -0600

    1

--- inCrawler/inCrawler.py
+++ inCrawler/inCrawler.py
@@ -1,33 +1,35 @@
 import re, urllib, random, webbrowser, urllib2, sys
+import json
 
 def crawl (x):
 	global urlBank
 	global phishBank
 	
 	json_list = open('seed.json', 'rb')
-	phishList = json.load(json_seed)
+	phishList = json.load(json_list)
 	for row in phishList:
-		phishListParsed = row["url"]
-	
+		phishListParsed += row['url']
+		print row['url'] + '\n'
 	try:
 		for i in re.findall('''href=["'](.[^"']+)["']''', urllib2.urlopen(x).read(200000), re.I):
-			# print i
 			z = re.match('http://' , i)
 			if z:
+				print i
 				urlBank.append(i)
-				for row in phishListParsed
+				for row in phishListParsed:
+					print "i is " + i + "\n"
+					print "row is " + row + "\n"
 					y = re.search(row , i)
 					if y:
 						phishBank.append(i)
-						print("found! pdfBank length is %d" %len(pdfBank))
+						print("found! pdfBank length is %d" %len(phishBank))
 		#print ("There are now %d items in urlBank" %len(urlBank))
 	except:
 		return
 
 urlBank = ['http://www.dmoz.org/World/Ukrainian/','http://www.dmoz.org/World/Arabic/','http://www.dmoz.org/World/Norsk/','http://www.dmoz.org/World/Catal%C3%A0/','http://www.dmoz.org/World/Esperanto/','http://www.dmoz.org/World/Rom%C3%A2n%C4%83/','http://www.dmoz.org/World/Suomi/','http://en.wikipedia.org/wiki/Special:Randompage', 'http://es.wikipedia.org/wiki/Special:Randompage',  'http://de.wikipedia.org/wiki/Special:Randompage' ,  'http://zh.wikipedia.org/wiki/Special:Randompage' ,  'http://pt.wikipedia.org/wiki/Special:Randompage',  'http://pl.wikipedia.org/wiki/Special:Randompage',  'http://it.wikipedia.org/wiki/Special:Randompage',  'http://ja.wikipedia.org/wiki/Special:Randompage',  'http://ru.wikipedia.org/wiki/Special:Randompage']
-pdfBank = []
+phishBank = []
 done = 0
-num = int(sys.argv[1])
 
 while done ==0 :
 	if len(urlBank) > 1000:
@@ -36,12 +38,12 @@ while done ==0 :
 	print ("CRAWLING " + urlBank[x] + "\n")
 	crawl(urlBank.pop(x))
 
-	if len(pdfBank) >= num:
-		pdfList = open('output.txt', 'w')
+	if len(phishBank) >= 3:
+		phishList = open('output.txt', 'w')
 		done = 1
-		for i in pdfBank:	
-			pdfList.write("%s \n" % i)
-		pdfList.close()
+		for i in phishBank:	
+			phishList.write("%s \n" % i)
+		phishList.close()
 
 print ("finished!")
 
