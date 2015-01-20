924f57d9bbb367cc465810dc769da4640f092376
codybraun <codybraun@gmail.com>
2015-01-19 19:18:09 -0600

    f

--- inCrawler/inCrawler.py
+++ inCrawler/inCrawler.py
@@ -17,24 +17,29 @@ while done == 0:
 	x = random.randrange(0, len(urlBank))
 	print ("CRAWLING " + urlBank[x] + "\n")
 	crawl = urlBank.pop(x)
+	try: 
+		if len(phishBank) >= 3:
+			phishList = open('output.txt', 'w')
+			done = 1
+			for i in phishBank:	
+				phishList.write("%s \n" % i)
+			phishList.close()
 
-	if len(phishBank) >= 3:
-		phishList = open('output.txt', 'w')
-		done = 1
-		for i in phishBank:	
-			phishList.write("%s \n" % i)
-		phishList.close()
-
-	for i in re.findall('''href=["'](.[^"']+)["']''', urllib2.urlopen(crawl).read(200000), re.I):
-		z = re.match('http://' , i)
-		if z:
-			print i
-			urlBank.append(i)
-			reqURL = "https://sb-ssl.google.com/safebrowsing/api/lookup?client=f4p&key=AIzaSyCD0pNAG-6HVh_W6udGYZFz-2_p0yHDD5k&appver=31&pver=3.1&url=" + i
-			response = urllib2.urlopen(reqURL).getcode()
-			if (response==200):
-				print ("FOUND %s" %i)
-				phishBank += i
+		for i in re.findall('''href=["'](.[^"']+)["']''', urllib2.urlopen(crawl).read(200000), re.I):
+			z = re.match('http://' , i)
+			if z:
+				print i
+				urlBank.append(i)
+				reqURL = "https://sb-ssl.google.com/safebrowsing/api/lookup?client=f4p&key=AIzaSyCD0pNAG-6HVh_W6udGYZFz-2_p0yHDD5k&appver=31&pver=3.1&url=" + i
+				response = urllib2.urlopen(reqURL).getcode()
+				if (response==200):
+					print ("FOUND %s" %i)
+					phishBank += i
+			
+	except Exception as e:
+		print ("Broken link to %s" %url)	
+		print (type(e))
+		print (e.args)		
 			
 print ("finished!")
 
