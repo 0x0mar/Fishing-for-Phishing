b8886dcfa0c06bf1e82b3050ad52f154f194bf94
codybraun <codybraun@gmail.com>
2015-01-21 11:54:58 -0600

    a

--- inCrawler/inCrawler.py
+++ inCrawler/inCrawler.py
@@ -1,5 +1,6 @@
 import re, urllib, random, webbrowser, urllib2, sys
 import json
+import MySQLdb
 
 #phishListParsed = []
 #json_list = open('seed.json', 'rb')
@@ -7,24 +8,20 @@ import json
 #for row in phishList:
 #	phishListParsed.append(row['url'])
 
-urlBank = ['http://www.solarmovie.eu','http://www.weather.com']
+db = MySQLdb.connect(host='cspp53001.cs.uchicago.edu',db='jcbraunDB',user='jcbraun',passwd='3312crystal')
+cursor = db.cursor()
+
+urlBank = ['http://www.solarmovie.eu','http://www.weather.com', 'https://kickass.so/', 'http://www.popularart.be/?option=com_k2&view=itemlist&task=user&id=1110']
 phishBank = []
 done = 0
 
 while done == 0:
-	if len(urlBank) > 10000:
-		urlBank [:100]
+	if len(urlBank) > 1000:
+		urlBank = ['http://www.solarmovie.eu','http://www.weather.com', 'https://kickass.so/', 'http://www.popularart.be/?option=com_k2&view=itemlist&task=user&id=1110']
 	x = random.randrange(0, len(urlBank))
 	print ("CRAWLING " + urlBank[x] + "\n")
 	crawl = urlBank.pop(x)
 	try: 
-		if len(phishBank) >= 3:
-			phishList = open('output.txt', 'w')
-			done = 1
-			for i in phishBank:	
-				phishList.write("%s \n" % i)
-			phishList.close()
-
 		for i in re.findall('''href=["'](.[^"']+)["']''', urllib2.urlopen(crawl).read(200000), re.I):
 			z = re.match('http://' , i)
 			if z:
@@ -34,6 +31,8 @@ while done == 0:
 				if (response==200):
 					print ("FOUND %s" %i)
 					phishBank += i
+					execString = ("INSERT INTO Content (Lvl, Content, Domain, URL, CopySource) VALUES ('0', '%s', '%s', '%s', 'crawl');" % (content, domain, url)) 
+					cursor.execute(execString)
 			
 	except Exception as e:
 		print ("Broken link to %s" %i)	
